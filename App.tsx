23ac34970c3d4f9ce421bae2e9c53e8b14f910d7
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2024-08-10 20:52:06 -0300

    feat: configuração das rotas do projeto e criado sceen login + view model

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Agora vai!</Text>
+      <Text>OVaifg!</Text>
       <StatusBar style="auto" />
     </View>
   );
